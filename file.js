7e3ab8cb63105b229a8515b137b2ab362642b697
Shubham Yadav <55327842+shubby12@users.noreply.github.com>
2022-09-01 16:57:48 +0530

    Shifted files

---
+++ file.js
@@ -0,0 +1,70 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let cactus = document.querySelector("#cactus");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
+
+
+//function for score
+let scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+
+//start Game
+window.addEventListener("keydown", (start) => {
+    //    console.log(start);
+    if (start.code == "Space") {
+        gameOver.style.display = "none";
+        cactus.classList.add("cactusActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+
+        //score
+        let playerScore = 0;
+        interval = setInterval(scoreCounter, 200);
+    }
+});
+
+
+//jump Your Character
+window.addEventListener("keydown", (e) => {
+    //    console.log(e);
+
+    if (e.key == "ArrowUp")
+        if (dino.classList != "dinoActive") {
+            dino.classList.add("dinoActive");
+
+            //                remove class after 0.5 seconds
+            setTimeout(() => {
+                dino.classList.remove("dinoActive");
+            }, 500);
+        }
+});
+
+//'Game Over' if 'Character' hit The 'cactus' 
+let result = setInterval(() => {
+    let dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
+    //    console.log("dinoBottom" + dinoBottom);
+
+    let cactusLeft = parseInt(getComputedStyle(cactus).getPropertyValue("left"));
+    //    console.log("cactusLeft" + cactusLeft);
+
+    if (dinoBottom <= 90 && cactusLeft >= 20 && cactusLeft <= 145) {
+        //        console.log("Game Over");
+
+        gameOver.style.display = "cactus";
+        cactus.classList.remove("cactusActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
+    }
+}, 10);
